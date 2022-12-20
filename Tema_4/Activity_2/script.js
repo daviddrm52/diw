af6588261bfb30837255fed2d8f39f16fcb5c4b8
daviddrm52 <davidgranada2015@gmail.com>
2022-12-20 14:38:10 +0100

    Activity 2 almost finished

--- Tema_4/Activity_2/script.js
+++ Tema_4/Activity_2/script.js
@@ -8,63 +8,80 @@ $("#buttonOutcome").click(function() {
     //Comprobation of the first input
     if(isNaN(num1)){
         titleOutcome = "";
-        subtitleOutcome = "";
-        errorMessageNum1 = "<p> ERROR: The first input doesn't have a number, write a number please! </p>";
         $('#titleOutcome').html(titleOutcome);
+        subtitleOutcome = "";
         $('#subtitleOutcome').html(subtitleOutcome);
+        infoMessage = "";
+        $('#infoMessage').html(infoMessage);
+        errorMessageNum1 = "ERROR: The first input doesn't have a number, write a number please!";
         $('#errorNum1').html(errorMessageNum1);
+        $('#errorNum1').bind("mouseenter mouseleave", function() {
+            $(this).toggleClass("errorMessage");
+        });
     } else if(num1 == ""){
         titleOutcome = "";
-        subtitleOutcome = "";
-        errorMessageNum1 = "<p> ERROR: The first input is empty, write a number please! </p>";
         $('#titeOutcome').html(titleOutcome);
+        subtitleOutcome = "";
         $('#subtitleOutcome').html(subtitleOutcome);
+        infoMessage = "";
+        $('#infoMessage').html(infoMessage);
+        errorMessageNum1 = "ERROR: The first input is empty, write a number please!";
         $('#errorNum1').html(errorMessageNum1);
+        $('#errorNum1').bind("mouseenter mouseleave", function() {
+            $(this).toggleClass("errorMessage");
+        });
     } else {
         isNum1aNumber = true;
         errorMessageNum1 = "";
         $('#errorNum1').html(errorMessageNum1);
     }
-    $('#errorNum1').bind("mouseenter mouseleave", function() {
-        $(this).toggleClass("errorMessage");
-    });
     //Variable for the second input
     var num2 = $('#num2').val();
     var isNum2aNumber = false;
     //Comprobation of the last input
     if(isNaN(num2)){
         titleOutcome = "";
-        subtitleOutcome = "";
-        errorMessageNum2 = "<p> ERROR: The second input doesn't have a number, write a number please! </p>";
         $('#titeOutcome').html(titleOutcome);
+        subtitleOutcome = "";
         $('#subtitleOutcome').html(subtitleOutcome);
+        infoMessage = "";
+        $('#infoMessage').html(infoMessage);
+        errorMessageNum2 = "ERROR: The second input doesn't have a number, write a number please!";
         $('#errorNum2').html(errorMessageNum2);
+        $('#errorNum2').bind("mouseenter mouseleave", function() {
+            $(this).toggleClass("errorMessage");
+        });
     } else if(num2 == ""){
         titleOutcome = "";
-        subtitleOutcome = "";
-        errorMessageNum2 = "<p> ERROR: The second input is empty, write a number please! </p>";
         $('#titleOutcome').html(titleOutcome);
+        subtitleOutcome = "";
         $('#subtitleOutcome').html(subtitleOutcome);
+        infoMessage = "";
+        $('#infoMessage').html(infoMessage);
+        errorMessageNum2 = "ERROR: The second input is empty, write a number please!";
         $('#errorNum2').html(errorMessageNum2);
+        $('#errorNum2').bind("mouseenter mouseleave", function() {
+            $(this).toggleClass("errorMessage");
+        });
     } else {
         isNum2aNumber = true;
         errorMessageNum2 = "";
         $('#errorNum2').html(errorMessageNum2);
     }
-    $('#errorNum2').bind("mouseenter mouseleave", function() {
-        $(this).toggleClass("errorMessage");
-    });
     if(isNum1aNumber == true && isNum2aNumber == true){
         $('#num1').val('');
         $('#num2').val('');
         var outcome = parseInt(num1) + parseInt(num2);
         titleOutcome = "Results";
         subtitleOutcome = "The outcome of "+num1+" + "+num2+" is: "+outcome;
+        infoMessage = "INFO: You have entered into the inputs "+num1+" & "+num2+".";
         $('#titleOutcome').html(titleOutcome);
         $('#subtitleOutcome').html(subtitleOutcome);
-        
-
-        $('#subtitleOutcome').bind("mouseenter mouseleave", function() {
+        $('#infoMessage').html(infoMessage);
+        $('#infoMessage').bind("mouseenter mouseleave", function() {
+            $(this).toggleClass("successMessage");
+        });
+        $('#userInfo').bind("mouseenter mouseleave", function() {
             $(this).toggleClass("successMessage");
         });
     }
