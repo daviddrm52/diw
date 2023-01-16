b23056059399456a80f7a2f1d83908a4cee34c3f
daviddrm52 <davidgranada2015@gmail.com>
2023-01-16 21:13:34 +0100

    avanzando actividad 4, no pinta bien kek

--- Tema_4/Activity_4_DavidRuedaMadrid/script.js
+++ Tema_4/Activity_4_DavidRuedaMadrid/script.js
@@ -1,42 +1,52 @@
 $( function() {
+  //Zona de los post-it rojos
   $( "#post-it-rojo" ).draggable();
-  $( "#post-it-rojo" ).resizable();
+  $( "#post-it-rojo" ).data("classified", false);
+  $( "#post-it-rojo" ).data("counter", 0);
   $( "#contenedor-rojo" ).droppable({
     drop: function( event, ui ) {
-      $( this )
-        .addClass( "ui-state-highlight" )
-        .find( "p" )
-          .html( "Post-it rojo entregado!" );
+      if(!ui.draggable.data("classified")){
+        counter = $(this).data("counter");
+        counter++;
+        $(this).data("counter", counter);
+        ui.draggable.data("classified", true);
+        $.html("Tienes "+($(this).data("counter"))+" post-it aparcados.");
+    
+      }  
     }
   });
-} );
 
-$( function() {
+  //Zona de los post-it naranjas
   $( "#post-it-naranja" ).draggable();
-  $( "#post-it-naranja" ).resizable();
   $( "#contenedor-naranja" ).droppable({
     drop: function( event, ui ) {
       $( this )
-        .addClass( "ui-state-highlight" )
-        .find( "p" )
-          .html( "Post-it naranja entregado!" );
+        .html("Has entregado un post-it");
     }
   });
-} );
 
-$(function () {
-    $( "#post-it" ).dialog({
+  $( "#btn-max-rojo, #btn-max-naranja" ).on( "click", function( event ) {
+    event.preventDefault();
+    console.log("bing chiling");
+  } );
+
+  $("#btn-close-rojo, #btn-close-naranja").on( "click", function( event ) {
+    $( "#dialog-confirm" ).dialog({
       resizable: false,
       height: "auto",
       width: 400,
       modal: true,
       buttons: {
-        "Delete all items": function() {
+        "Eliminar": function() {
           $( this ).dialog( "close" );
+          $( this ).parent().remove();
         },
-        Cancel: function() {
+        "Cancelar": function() {
           $( this ).dialog( "close" );
         }
       }
     });
   } );
+} );
+
+
