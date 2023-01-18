4361edfa077c81bdcd44a10ad556032ce433acdc
daviddrm52 <davidgranada2015@gmail.com>
2023-01-18 15:04:40 +0100

    Avanzando actividad 4, de momento, funciona

--- Tema_4/Activity_4_DavidRuedaMadrid/script.js
+++ Tema_4/Activity_4_DavidRuedaMadrid/script.js
@@ -1,3 +1,4 @@
+var counter = 0;
 $( function() {
   //Zona de los post-it rojos
   $( "#post-it-rojo" ).draggable();
@@ -10,18 +11,28 @@ $( function() {
         counter++;
         $(this).data("counter", counter);
         ui.draggable.data("classified", true);
-        $.html("Tienes "+($(this).data("counter"))+" post-it aparcados.");
-    
+        $(this).html("You have "+($(this).data("counter"))+" post-it stored.");
       }  
     }
   });
 
   //Zona de los post-it naranjas
-  $( "#post-it-naranja" ).draggable();
+  $("#post-it-naranja").draggable().click(function() {
+    $(this).draggable( {disabled: false});
+    }).dblclick(function() {
+    $(this).draggable({ disabled: true });
+  });  
+  $( "#post-it-naranja" ).data("classified", false);
+  $( "#post-it-naranja" ).data("counter", 0);
   $( "#contenedor-naranja" ).droppable({
     drop: function( event, ui ) {
-      $( this )
-        .html("Has entregado un post-it");
+      if(!ui.draggable.data("classified")){
+        counter = $(this).data("counter");
+        counter++;
+        $(this).data("counter", counter);
+        ui.draggable.data("classified", true);
+        $(this).html("You have "+($(this).data("counter"))+" post-it stored.");
+      }  
     }
   });
 
@@ -30,23 +41,52 @@ $( function() {
     console.log("bing chiling");
   } );
 
-  $("#btn-close-rojo, #btn-close-naranja").on( "click", function( event ) {
+  $("#btn-close-naranja").on( "click", function( event ) {
     $( "#dialog-confirm" ).dialog({
       resizable: false,
       height: "auto",
       width: 400,
       modal: true,
       buttons: {
-        "Eliminar": function() {
+        "Delete": function() {
+          $("#post-it-naranja").remove();
           $( this ).dialog( "close" );
-          $( this ).parent().remove();
         },
-        "Cancelar": function() {
+        "Cancel": function() {
           $( this ).dialog( "close" );
         }
       }
     });
-  } );
-} );
+  });
+
+  $("#btn-close-rojo").on( "click", function( event ) {
+    $( "#dialog-confirm" ).dialog({
+      resizable: false,
+      height: "auto",
+      width: 400,
+      modal: true,
+      buttons: {
+        "Delete": function() {
+          $("#post-it-rojo").remove();
+          $( this ).dialog( "close" );
+        },
+        "Cancel": function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+  });
+
+  $( "#btn-create-naranja" ).on( "click", function( event ) {
+    console.log("hung tao");
+    jQuery('<div>', {
+      id: 'post-it-naranja',
+      class: 'post-it-naranja'
+    }).appendTo('.post-it-area');
+    $('.post-it-naranja').append('<button id="btn-close-naranja" style="float: right; margin: 2px;"><span class="material-symbols-outlined">close</span></button>');
+    $('.post-it-naranja').append('<button id="btn-min-naranja" style="float: right; margin: 2px;"><span class="material-symbols-outlined">minimize</span></button>');
+    $('.post-it-naranja').append('<p> </p>');
+  });
+});
 
 
